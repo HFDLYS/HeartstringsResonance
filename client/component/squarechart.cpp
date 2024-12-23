52d50c1e2dafb1cbd3551100d60c425ec5f57bfd
HFDLYS <845388925@qq.com>
2024-12-23 22:30:57 +0800

    feat: 更新方形的饼状图

---
+++ client/component/squarechart.cpp
@@ -0,0 +1,33 @@
+#include "squarechart.h"
+
+void SquarePieChart::setValues(int index, int value) {
+    values[index] = value;
+    update();
+}
+
+void SquarePieChart::paintEvent(QPaintEvent* event) {
+    QPainter painter(this);
+    painter.setRenderHint(QPainter::Antialiasing);
+
+    QRectF squareRect(0, 0, width(), height());
+    painter.setClipRect(squareRect);
+    QVector<QColor> colors = {Qt::red, Qt::blue, Qt::yellow, Qt::cyan};
+    double side = 16 * qMax(width(), height());
+    QRectF bigCircleRect(
+        (width() - side) / 2.0,   // 居中一下
+        (height() - side) / 2.0,
+        side,
+        side
+    );
+    double sum = 0;
+    for (int i = 1; i <= 4; i++) {
+        sum += values[i];
+    }
+    double startAngle = 0;
+    for (int i = 1; i <= 4; i++) {
+        double angle = values[i] / sum * 360;
+        painter.setBrush(QBrush(colors[i - 1]));
+        painter.drawPie(bigCircleRect, startAngle * 16, angle * 16);
+        startAngle += angle;
+    }
+}
---
+++ client/component/squarechart.h
@@ -0,0 +1,30 @@
+#ifndef SQUARECHART_H
+#define SQUARECHART_H
+
+#include <QWidget>
+#include <QPainter>
+#include <QPen>
+#include <QBrush>
+#include <QRectF>
+
+class SquarePieChart : public QWidget {
+    Q_OBJECT
+
+public:
+    SquarePieChart(QWidget* parent = nullptr) : QWidget(parent) {
+        values[0] = 0;
+        values[1] = 1;
+        values[2] = 1;
+        values[3] = 1;
+        values[4] = 1;
+    }
+
+    void setValues(int index, int value);
+protected:
+    void paintEvent(QPaintEvent* event) override;
+
+private:
+    int values[5];
+};
+
+#endif // SQUARECHART_H
