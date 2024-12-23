442e7a62f79525ec1ae55f6aaa5e339b8cfd77e0
HFDLYS <845388925@qq.com>
2024-12-23 13:52:37 +0800

    feat: 新增dlc，动物乐园

--- client/graphics/gemmodelfactory.cpp
+++ client/graphics/gemmodelfactory.cpp
@@ -4,10 +4,10 @@ namespace Graphics {
 
 GemModelFactory::GemModelFactory(QOpenGLFunctions_3_3_Core* func) {
     models_.push_back(new Model(func, "./resource/models/gem/gemstone_1.obj"));
-    models_.push_back(new Model(func, "./resource/models/gem/love.obj"));
-    models_.push_back(new Model(func, "./resource/models/gem/yf.obj"));
-    models_.push_back(new Model(func, "./resource/models/gem/flower.obj"));
-    models_.push_back(new Model(func, "./resource/models/gem/diamond.obj"));
+    models_.push_back(new Model(func, "./resource/models/gem/yuyu.obj"));
+    models_.push_back(new Model(func, "./resource/models/gem/doge.obj"));
+    models_.push_back(new Model(func, "./resource/models/gem/hachimi.obj"));
+    models_.push_back(new Model(func, "./resource/models/gem/pig.obj"));
     models_.push_back(new Model(func, "./resource/models/gem/blue.obj"));
     models_.push_back(new Model(func, "./resource/models/gem/gemstone_6.obj"));
     models_.push_back(new Model(func, "./resource/models/gem/gemstone_7.obj"));
