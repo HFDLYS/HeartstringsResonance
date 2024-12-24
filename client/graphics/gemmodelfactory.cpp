68925a498aea37db7d4c677e66ea1311e7f20a37
HFDLYS <845388925@qq.com>
2024-12-25 07:59:42 +0800

    feat: 宝石风格可切换

--- client/graphics/gemmodelfactory.cpp
+++ client/graphics/gemmodelfactory.cpp
@@ -1,13 +1,30 @@
 #include "gemmodelfactory.h"
 
 namespace Graphics {
+const int MAX_STYLE = 2;
+
+const QVector<std::string> STYLE[MAX_STYLE] =  {
+    {
+        "diamond.obj",
+        "yf.obj",
+        "love.obj",
+        "flower.obj",
+    },
+    {
+        "yuyu.obj",
+        "doge.obj",
+        "hachimi.obj",
+        "pig.obj",
+    }
+};
 
 GemModelFactory::GemModelFactory(QOpenGLFunctions_3_3_Core* func) {
+    int style = rand() % MAX_STYLE;
     models_.push_back(new Model(func, "./resource/models/gem/gemstone_1.obj"));
-    models_.push_back(new Model(func, "./resource/models/gem/yuyu.obj"));
-    models_.push_back(new Model(func, "./resource/models/gem/doge.obj"));
-    models_.push_back(new Model(func, "./resource/models/gem/hachimi.obj"));
-    models_.push_back(new Model(func, "./resource/models/gem/pig.obj"));
+    models_.push_back(new Model(func, "./resource/models/gem/" + STYLE[style][0]));
+    models_.push_back(new Model(func, "./resource/models/gem/" + STYLE[style][1]));
+    models_.push_back(new Model(func, "./resource/models/gem/" + STYLE[style][2]));
+    models_.push_back(new Model(func, "./resource/models/gem/" + STYLE[style][3]));
     models_.push_back(new Model(func, "./resource/models/gem/blue.obj"));
     models_.push_back(new Model(func, "./resource/models/gem/gemstone_2.obj"));
     models_.push_back(new Model(func, "./resource/models/gem/gemstone_7.obj"));
